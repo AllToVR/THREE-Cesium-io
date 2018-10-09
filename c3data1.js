e5675e0f78e69592ebc612bf87c920f297630424
Rue LaRue <43354207+AllToVR@users.noreply.github.com>
2018-10-09 14:39:35 -0400

    changed file location to a generic reference

--- c3data1.js
+++ c3data1.js
@@ -3,15 +3,15 @@ var CRLF = '\n'
 var fs = require('fs-extra')
 //
 exports.sendFullSampleZip = function ( socket){
-	var contents = fs.readFileSync('C:/HostedSites/mib/apps/irl/THREECesumExample.zip')
+	var contents = fs.readFileSync('C:/YourServer/yourJavaScriptFolder/THREECesumExample.zip')
 	socket.emit('doneCZML', {my:contents}, 'THREECesumExample.zip')
 	var now = new Date();
 	console.log("*************** sendFullSampleZip: " + now.toString() )
 }
 //
 exports.sendSampleZip = function ( socket){
-	var contents = fs.readFileSync('C:/HostedSites/mib/apps/irl/testdata/chartPalResults.zip')
-	socket.emit('doneCZML', {my:contents}, 'chartPalResults.zip')
+	var contents = fs.readFileSync('C:/YourServer/yourJavaScriptFolder/chartResults.zip')
+	socket.emit('doneCZML', {my:contents}, 'chartResults.zip')
 	var now = new Date();
 	console.log("******************* sendSampleZip: " + now.toString() )
 }
