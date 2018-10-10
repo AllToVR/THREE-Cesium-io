1cfc5242c3b281a06230bf238f5173e7df57eecd
Rue LaRue <43354207+AllToVR@users.noreply.github.com>
2018-10-10 12:04:09 -0400

    added load3ZMLData function

--- c3data1.js
+++ c3data1.js
@@ -3,16 +3,22 @@ var CRLF = '\n'
 var fs = require('fs-extra')
 //
 exports.sendFullSampleZip = function ( socket){
-	var contents = fs.readFileSync('C:/YourServer/yourJavaScriptFolder/THREECesumExample.zip')
-	socket.emit('doneCZML', {my:contents}, 'THREECesumExample.zip')
-	var now = new Date();
-	console.log("*************** sendFullSampleZip: " + now.toString() )
+	var contents = fs.readFileSync('C:/YourServer/yourDataFolder/someFile.zip')
+	socket.emit('doneCZML', {my:contents}, 'someFile.zip')
 }
 //
 exports.sendSampleZip = function ( socket){
-	var contents = fs.readFileSync('C:/YourServer/yourJavaScriptFolder/chartResults.zip')
+	var contents = fs.readFileSync('C:/YourServer/yourDataFolder/chartResults.zip')
 	socket.emit('doneCZML', {my:contents}, 'chartResults.zip')
-	var now = new Date();
-	console.log("******************* sendSampleZip: " + now.toString() )
 }
 //
+//
+exports.load3ZMLData = function (nameToLoad, position, source, socket){
+	fs.readFile('c:/YourServer/yourDataFolder/chartresults/'+nameToLoad+'.3zml', 'utf8', doCallBack( position, source, socket))
+	function doCallBack( position, source, socketToUse){
+		var CallBack = function(err, origPageData) {
+			socketToUse.emit('loadSampleZip', origPageData, position, source, nameToLoad)
+		}
+		return CallBack
+	}
+}
